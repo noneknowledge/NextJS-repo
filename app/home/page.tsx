11f8d9064ddb2f5b7195ea91ee96e99ec65dd934
unknown <hoangphuc160802@gmai.com>
2024-06-25 16:32:16 +0700

    header footer

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+const HomePage = () =>{
+    return(<>
+        <h1 className="text-red-500">HomePage</h1>
+    </>)
+}
+
+
+export default HomePage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { images: {
+    domains: ['images.unsplash.com','tailwindui.com'],
+},};
 
 export default nextConfig;
